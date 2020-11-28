5b34a5fd2d7714664299a0445ab2fafac4ccbc91
Jobggun <jobggun@kakao.com>
2020-11-28 16:56:26 +0900

    up9

--- Web/steamid.py
+++ Web/steamid.py
@@ -1,25 +1,25 @@
-from json
+import json
 from browser import document, html, ajax, bind, console
 from browser.widgets.dialog import Dialog, InfoDialog
 @bind("strong.class_chatlog", "click")
 def onclick(ev):
-	def on_complete(req):
+    def on_complete(req):
         if req.status == 200 or req.status == 0:
-            userdata = json.loads(req.text);
+            userdata = json.loads(req.text)
 
             name = userdata["name"]
             steamid = userdata["auth"]
 
             left = ev.x
-	        top = ev.y
+            top = ev.y
 
-	        d = Dialog("Name : " + name, ok_cancel=True);
+            d = Dialog("Name : " + name, ok_cancel=True)
 
-            d.panel <= html.DIV("SteamID : " + steamid);
-        else
+            d.panel <= html.DIV("SteamID : " + steamid)
+        else:
             InfoDialog("Error", "It is unable to connect to api server.")
     
-    req = ajax.get('./api.php', data={'msg_id': ev.currentTarget.id})
+    req = ajax.get('./api.php', data={'msg_id': ev.currentTarget.id}, oncomplete=on_complete)
 
 @bind(d.ok_button, "click")
 def ok(ev):
